2e58c1a37d45f10975308d5cff0bed94fb7c73fc
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-03-09 12:01:37 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
+}
+
+variable "description" {
+  type        = string
+  default     = "Event rule to invoke lambda"
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "schedule_expression" {
+  type        = string
+  default     = "cron schedule"
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "arn" {
+  type        = string
+  default     = "lambda arn"
+  description = "Name  (e.g. `app` or `cluster`)."
+}
+
+variable "function_name" {
+  type        = string
+  default     = "lambda fun name"
+  description = "Name  (e.g. `app` or `cluster`)."
+}
